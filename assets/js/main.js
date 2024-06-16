6f3aac214d5df21f1adb52fc011138a1cca5e7e3
bth <gh@hautogdoad.com>
2024-06-16 15:15:38 -0700

    add captureOutboundLink for GA link click tracking

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,6 +24,20 @@ const listen = (ele, e, callback) => {
   }
 }
 
+// https://support.google.com/analytics/answer/1136920
+/**
+* Function that captures a click on an outbound link in Analytics.
+* This function takes a valid URL string as an argument, and uses that URL string
+* as the event label. Setting the transport method to 'beacon' lets the hit be sent
+* using 'navigator.sendBeacon' in browser that support it.
+*/
+var captureOutboundLink = function(url) {
+   ga('send', 'event', 'outbound', 'click', url, {
+     'transport': 'beacon',
+     'hitCallback': function(){document.location = url;}
+   });
+}
+
 /**
  * Functions
  */
