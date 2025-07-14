a5df93bd8d383c2293200a4b4518e727fffd38c2
bth <gh@hautogdoad.com>
2025-07-14 15:52:46 -0700

    initial draft of themeToggle : works, but flicker on first use because it doesnt load. and not persistent

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,10 +69,36 @@ const toggleWorksMenu = () => {
   }
 }
 
+const toggleTheme = () => {
+    let link = document.getElementById('theme-link');
+
+    let theme_current = link.getAttribute('data-current-theme');
+    let theme_set = link.getAttribute('data-set-theme');
+    let theme_light = link.getAttribute('data-light-theme');
+
+    // we can't just use the href as key for the if block because it's fingerprinted.
+    let theme_set_href = link.getAttribute('data-set-theme-path');
+    let theme_light_href = link.getAttribute('data-light-theme-path');
+
+    let theme_set_integ = link.getAttribute('data-set-integrity');
+    let theme_light_integ = link.getAttribute('data-light-integrity');
+
+    if (theme_current == theme_set) {
+        link.setAttribute('data-current-theme', theme_light);
+        link.setAttribute('integrity', theme_light_integ);
+        link.setAttribute('href', theme_light_href);
+    } else {
+        link.setAttribute('data-current-theme', theme_set);
+        link.setAttribute('integrity', theme_set_integ);
+        link.setAttribute('href', theme_set_href);
+    }
+}
+
 // Listeners : actual function invocation
 
 if (header !== null) {
   listen('#menu-btn', "click", toggleWorksMenu);
+  listen('#theme-toggle-btn', "click", toggleTheme);
 
   window.addEventListener('scroll', throttle(() => {
 
