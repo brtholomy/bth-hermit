89a3d35cfc340d0aaf883ecc9e1ece7061a6dcc3
bth <gh@hautogdoad.com>
2026-03-10 13:36:43 -0700

    clean up js : delete gtagConversionEventOutboundClick

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,13 @@
-/**
- * Utils
- */
-
-// addEventListener Helper
-//
 const listen = (ele, e, callback) => {
   if (document.querySelector(ele) !== null) {
     document.querySelector(ele).addEventListener(e, callback);
   }
 }
 
-// Google Ads generated this.
-function gtagConversionEventOutboundClick(url) {
-    // Helper function to delay opening a URL until a gtag event is sent.
-    // Call it in response to an action that should navigate to a URL.
-    var callback = function () {
-        if (typeof url === 'string') {
-            window.location = url;
-        }
-    };
-    gtag('event', 'conversion_event_outbound_click', {
-        'event_callback': callback,
-        'event_timeout': 2000,
-        // added param:
-        'url': url,
-    });
-    // NOTE: this will prevent default behavior, meaning not navigate.
-    return false;
-}
-
-/**
- * Functions
- */
-
 const toggleTheme = () => {
     let link = document.getElementById('light-theme-link');
     link.disabled = !link.disabled;
 }
-
-// Listeners : actual function invocation
 
 let header = document.getElementById('site-header');
 if (header !== null) {
